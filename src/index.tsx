2080587153becd3eddba4f50bd2658576dafd9ef
Christian Koch Echeverria <ko.che@web.de>
2024-08-27 13:20:11 +0200

    try 15

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
+import { remapProps } from "nativewind";
 import tailWindConfig from "./ui/tailwind.config";
+import { Button, Paragraph, View } from "tamagui";
+remapProps(View, { className: "style" });
+remapProps(Button, { className: "style" });
+remapProps(Paragraph, { className: "style" });
 export * from "./ui/add-button";
 export * from "./ui/confirmation-dialog";
 export * from "./ui/divider";
