55f75b8e422e116e1ef5dffc82a45113ab1656f3
Christian Koch Echeverria <ko.che@web.de>
2024-08-27 08:06:51 +0200

    try 9

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  presets: [
-    ['module:react-native-builder-bob/babel-preset', { modules: 'commonjs' }],
-  ],
+	presets: [
+		[
+			"module:react-native-builder-bob/babel-preset",
+			{ modules: "commonjs" },
+		],
+	],
+	plugins: [
+		"@tamagui/babel-plugin",
+		{
+			config: "./tamagui.config.ts",
+			components: ["./src/ui", "./src/theme"],
+		},
+	],
 };
