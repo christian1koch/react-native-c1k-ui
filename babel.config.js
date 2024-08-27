fd53d43c37af9fb5de31af0a6acf22914bae4ecd
Christian Koch Echeverria <ko.che@web.de>
2024-08-27 13:12:19 +0200

    try idk

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
-module.exports = {
-	presets: [
-		["module:react-native-builder-bob/babel-preset", { modules: "es" }],
-	],
-	plugins: ["nativewind/babel"],
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: [
+			["babel-preset-expo", { jsxImportSource: "nativewind" }],
+			"nativewind/babel",
+			["module:react-native-builder-bob/babel-preset", { modules: "es" }],
+		],
+	};
 };
--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-	content: ["./src/**/*.{js,jsx,ts,tsx}", "./example/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-} satisfies Config;
