4e2e5fe8391b79664eb0f6bf689f7af39a6d472f
Christian Koch Echeverria <ko.che@web.de>
2024-08-27 09:11:32 +0200

    ty 14

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,3 @@ export * from "./ui/add-button";
 export * from "./ui/confirmation-dialog";
 export * from "./ui/divider";
 export * from "./ui/header-nav";
-export * from "./ui/AppProvider";
-export { default as Config } from "../tamagui.config";
--- src/ui/AppTheme.tsx
+++
@@ -1,5 +0,0 @@
-import { config, tokens } from "@tamagui/config/v3";
-import { createTamagui } from "tamagui";
-import * as themes from "./../../theme/theme";
-
-export const tamaguiConfig = createTamagui({ ...config, tokens, themes });
