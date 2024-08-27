9a8ac82b7cd37c561b5251c9d8f784620bdc1b78
Christian Koch Echeverria <ko.che@web.de>
2024-08-27 11:09:57 +0200

    try 4 tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
+import tailWindConfig from "../tailwind.config";
 export * from "./ui/add-button";
 export * from "./ui/confirmation-dialog";
 export * from "./ui/divider";
 export * from "./ui/header-nav";
+
+export const config = tailWindConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
 	content: ["./src/**/*.{js,jsx,ts,tsx}", "./example/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
-};
+} satisfies Config;
