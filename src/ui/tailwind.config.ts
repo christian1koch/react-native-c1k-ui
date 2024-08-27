27b65015fb878653ec2a8f8532d419cc4903c9de
Christian Koch Echeverria <ko.che@web.de>
2024-08-27 11:15:38 +0200

    try 6

--- src/ui/tailwind.config.ts
+++ src/ui/tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "tailwindcss";
 
 export default {
-	content: ["./src/**/*.{js,jsx,ts,tsx}", "./example/**/*.{js,jsx,ts,tsx}"],
+	content: [
+		"../../src/**/*.{js,jsx,ts,tsx}",
+		"./example/**/*.{js,jsx,ts,tsx}",
+	],
 	theme: {
 		extend: {},
 	},
